113f6c3afd1f88a8898bdffe788573a399f4c377
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-07-29 18:41:49 -0300

    reaload github

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import productRouter from './routers/product';
 import userRouter from './routers/user';
+import orderRouter from './routers/order';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', productRouter);
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
 
 export default app;
---
+++ src/routers/order.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+// import UserController from '../controllers/user';
+
+// const controller = new UserController();
+
+const router = Router();
+
+router.post('/', () => { });
+
+export default router;
